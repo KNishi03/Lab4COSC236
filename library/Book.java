c677fa66b17a365877c1d880a3a1d500fb2ba8b2
Anshika252 <anshikakhullar231204@gmail.com>
2024-11-07 19:42:21 -0800

    Update Book.java

--- library/Book.java
+++ library/Book.java
@@ -5,8 +5,36 @@ public class Book {
 	// GRASP Principle: Information Expert
 	// Manages its own state (available or borrowed).
 	private String title;
-	private boolean isAvailable; 
+	private boolean isAvailable;
+	private String author;
+
 	
 	// TODO: implement Book functionality 
+	public Book(String title, String author){
+		this.title = title;
+		this.author = author;
+		this.isAvailable = true;
+	}
+	public String getTitle(){
+		return title;
+	}
+	public String getAuthor(){
+		return author;
+	}
+	public boolean isAvailable(){
+		return isAvailable;
+	}
+	public boolean borrowBook(){
+		if(isAvailable){
+			this.isAvailable = !isAvailable;
+			return true;
+		}else{
+			return false;
+		}
+		
+	}
+	public boolean returnBook(){
+		isAvailable=true;
+	}
 	
 }
